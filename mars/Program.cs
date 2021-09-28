2319d9c7423c1d8542087bddf3638b40d09a42eb
Yuri Melo <yurifullstack@gmail.com>
2021-09-27 22:24:05 -0300

    First files

---
+++ mars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace mars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var redis = RedisStore.RedisCache;
+
+            var sub = redis.Multiplexer.GetSubscriber();
+            
+            sub.Subscribe("connection-mars-to-earth", (channel, message) => {
+                 Console.WriteLine("Got notification: " + (string)message);
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
